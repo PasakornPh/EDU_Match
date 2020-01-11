4d468e5de3e742b3c07feb1bfc11ff442fedec10
Pasakorn Phareyart <s6101012630135@email.kmutnb.ac.th>
2020-01-12 02:19:34 +0700

    Functional test now checks we can input a Subject

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,14 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # He saw that the title of this website is EDU-Match
         self.assertIn('EDU-Match', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('EDU-Match', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_subject')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a Subject'
+        )
+
+        # He type the name of the subject that was not good
+        # He type "Statistics in everyday life" into the Text box
+        inputbox.send_keys('Statistics in everyday life')
+
+        # When he press Enter, the page shows the name of a person who is good at "Statistics in everyday life"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Statistics in everyday life' for row in rows)
+        )
+
         self.fail('Finish the test!')
-
-    # He type the name of the subject that was not good
-
-    # He type "Statistics in everyday life" into the Text box
-
-    # When he press Enter, the page shows the name of a person who is good at "Statistics in everyday life"
-
     # From then, he added the names of subjects that he was not good at anymore
 
     # He typed "General Mathematics"
