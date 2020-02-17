7cd719523cdda0e98dad6365bb80276cd9c641a2
Pasakorn Phareyart <s6101012630135@email.kmutnb.ac.th>
2020-02-18 00:03:53 +0700

    Server can send email to reset password when delete comment and file settings.py

--- match_edu/settings.py
+++ match_edu/settings.py
@@ -141,3 +141,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'pythonium.group@gmail.com'
+#EMAIL_HOST_PASSWORD = 'jpoafaknfkshoqgr'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#DEFAULT_FROM_EMAIL = 'Pythonium Group'
