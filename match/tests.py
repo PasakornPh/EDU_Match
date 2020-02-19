8586aab1eecdd2e667912c12e427e5c034f368ae
Pasakorn Phareyart <s6101012630135@email.kmutnb.ac.th>
2020-02-19 17:45:26 +0700

    Unitest Test SignUpTest ChangeFirstNameTest ChangePasswordTest

--- match/tests.py
+++ match/tests.py
@@ -26,3 +26,52 @@ class HumanModelTest(TestCase):
         self.assertEqual(all_user.count(),2)
         self.assertEqual(first_saved_user.subject.first().name, 'first_subject')
         self.assertEqual(second_saved_user.subject.first().name, 'second_subject')
+
+class SignUpTest(TestCase):
+    def test_user_signup(self):
+        self.user1 =  User.objects.create_user(username='testuser', password='Password12345', email='test@example.com')
+        self.user1.save()
+
+        users = User.objects.all()
+        self.assertEqual(users.count(), 1)
+
+    def test_two_user_signup(self):
+        self.user1 = User.objects.create_user(username='testuser', password='Password12345', email='test@example.com')
+        self.user1.save()
+
+        self.user2 = User.objects.create_user(username='testuser2', password='Password12345', email='test2@example.com')
+        self.user2.save()
+
+        users = User.objects.all()
+        self.assertEqual(users.count(), 2)
+
+class ChangeFirstNameTest(TestCase):
+    def test_change_first_name(self):
+        self.user1 = User.objects.create_user(username='testuser', first_name = 'firstname' , password='Password12345', email='test@example.com')
+        self.user1.save()
+
+        users = User.objects.get(username='testuser')
+
+        self.assertEqual(users.first_name,'firstname')
+
+        users.first_name = 'change_firstname'
+        users.save()
+
+        self.assertEqual(users.first_name, 'change_firstname')
+
+
+
+
+class ChangePasswordTest(TestCase):
+    def test_change_password(self):
+        self.user1 = User.objects.create_user(username='testuser', password='Password12345', email='test@example.com')
+        self.user1.save()
+
+        users = User.objects.get(username='testuser')
+        hash_password1 = users.password
+
+        users.set_password('Password67890')
+        users.save()
+
+        hash_password2 = users.password
+        self.assertNotEquals(hash_password1,hash_password2)
